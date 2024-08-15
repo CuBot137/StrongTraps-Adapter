92314e6c13ccc5f715abe5d03222d167a251caf0
CuBot137 <91560915+CuBot137@users.noreply.github.com>
2024-08-15 16:40:49 +0100

    Add files via upload

---
+++ StrongTrapsAdapter/StrongTrapsAdapter/src/test/java/com/java/GCP/StrongTrapsAdapter/StrongTrapsAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.GCP.StrongTrapsAdapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrongTrapsAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
