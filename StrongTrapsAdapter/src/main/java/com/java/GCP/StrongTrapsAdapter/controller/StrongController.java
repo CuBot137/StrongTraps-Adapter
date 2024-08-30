9776d89098bf946f7633b66f0e9e4f2a5847859b
conor <conorlynam1234@gmail.com>
2024-08-30 14:53:40 +0100

    post method created and tested

--- StrongTrapsAdapter/src/main/java/com/java/GCP/StrongTrapsAdapter/controller/StrongController.java
+++ StrongTrapsAdapter/src/main/java/com/java/GCP/StrongTrapsAdapter/controller/StrongController.java
@@ -1,6 +1,11 @@
 package com.java.GCP.StrongTrapsAdapter.controller;
 
+import com.java.GCP.StrongTrapsAdapter.model.TestModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +14,13 @@ public class StrongController {
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello, World!";
+    }
+
+    @PostMapping("/test-json")
+    public ResponseEntity<String> receiveTestData(@RequestBody TestModel testModel){
+        String name = testModel.getName();
+        int age = testModel.getAge();
+        System.out.println("Received test data: Name="+name+", Age="+age);
+        return new ResponseEntity<>("Person data received successfully", HttpStatus.OK);
     }
 }
--- StrongTrapsAdapter/src/test/java/com/java/GCP/StrongTrapsAdapter/StrongTrapsAdapterApplicationTests.java
+++ StrongTrapsAdapter/src/test/java/com/java/GCP/StrongTrapsAdapter/StrongTrapsAdapterApplicationTests.java
@@ -1,13 +1,43 @@
 package com.java.GCP.StrongTrapsAdapter;
 
+import com.java.GCP.StrongTrapsAdapter.controller.StrongController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(StrongController.class)
 class StrongTrapsAdapterApplicationTests {
+	//
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testHelloWorld() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
+	}
+
+	@Test
+	public void testRecieveTestData() throws Exception {
+		// Create JSON string to send in the request
+		String json = "{\"name\": \"John Doe\", \"age\": 30}";
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/test-json")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json))
+				.andExpect(status().isOk()) // Expect HTTP 200 OK
+				.andExpect(MockMvcResultMatchers.content().string("Person data received successfully")); // Expect specific response content
+	}
 }
